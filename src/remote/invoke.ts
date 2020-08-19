e450fa8fc71c1e32111712d3b0523d637e804d57
Rob Gordon <rob@rgordon.co.uk>
2020-08-19 12:18:57 +0100

    Invoke client poc

--- src/remote/invoke.ts
+++ src/remote/invoke.ts
@@ -1,4 +1,30 @@
 
+export class OperationType {
+
+    constructor(readonly name: string,
+        readonly type: string,
+        readonly signature: string[]) {
+
+    }
+}
+
+export class InvokeRequest {
+
+    constructor(readonly remoteId: number,
+        readonly operationType: OperationType,
+        readonly args: Object[]) {
+
+    }
+}
+
+const operationTypes: Map<String, OperationType> = new Map();
+
+
+export function invokeRequest() {
+
+
+}
+
 
 export class RemoteInvoker {
 
@@ -6,4 +32,4 @@ export class RemoteInvoker {
 
 }
 
-  
+
